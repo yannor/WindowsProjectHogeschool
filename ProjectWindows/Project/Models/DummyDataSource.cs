f79ed22bb683ae282698357571a465d988603f9e
Matthias Beerens <beerens.matthias@hotmail.com>
2016-11-29 12:12:34 +0100

    Test richting toegevoegd

---
+++ ProjectWindows/Project/Models/DummyDataSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class DummyDataSource
+    {
+
+        public static Richting RichtingToegepasteInformatica = new Richting()
+        {
+            Title="Toegepaste Informatica",
+            Description = "De IT’er van vandaag is veel meer dan alleen maar een technische expert. Hij of zij moet tegelijkertijd een ondernemende, communicatieve en klantgerichte teamspeler zijn. IT is namelijk geëvolueerd van een zuiver technisch verhaal naar ’IT as a service and support tool’."
+        };
+
+    }
+}
---
+++ ProjectWindows/Project/ViewModels/RichtingViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml.Navigation;
+using Project.Models;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+
+namespace Project.ViewModels
+{
+    public class RichtingViewModel : ViewModelBase
+    {
+        public RichtingViewModel()
+        {
+            Richting = DummyDataSource.RichtingToegepasteInformatica;
+        }
+
+        private Richting _richting; 
+        public Richting Richting {
+            get { return _richting; }
+            set { _richting = value;RaisePropertyChanged(); }
+        }
+        
+        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
+        {
+            
+            Richting = (suspensionState.ContainsKey(nameof(Richting))) ? (Richting)suspensionState[nameof(Richting)] : (Richting)parameter;
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
+        {
+            if (suspending)
+            {
+                suspensionState[nameof(Richting)] = Richting;
+
+            }
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
+        {
+            args.Cancel = false;
+            await Task.CompletedTask;
+        }
+
+    }
+}
