b5a35b3689f5618d7ca5baf274728b29e16b0a75
Matthias Beerens <beerens.matthias@hotmail.com>
2016-12-06 12:01:19 +0100

    Aanpassen naar informatica pagina

--- ProjectWindows/Project/Views/InformaticaPage.xaml.cs
+++ ProjectWindows/Project/Views/InformaticaPage.xaml.cs
@@ -22,9 +22,9 @@ namespace Project.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class RichtingPage : Page
+    public sealed partial class InformaticaPage : Page
     {
-        public RichtingPage()
+        public InformaticaPage()
         {
             this.InitializeComponent();
            NavigationCacheMode = NavigationCacheMode.Disabled;
