b573e7213d72be6d9a0b90d6e37a65380f2120cc
Yannick <yannick.servranckx@hotmail.com>
2016-12-06 10:05:51 +0100

    Test

---
+++ ProjectWindows/TryOut/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using TryOut.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace TryOut.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
