6fb7dc0e676cdf289b0a838063575048e7e3135b
Yannick <yannick.servranckx@hotmail.com>
2016-12-13 09:38:46 +0100

    Added klasse campus + dummydata

---
+++ ProjectWindows/Project/Models/Campus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Campus
+    {
+        public string Uitleg { get; set; }
+    }
+}
--- ProjectWindows/Project/Models/DummyDataSource.cs
+++ ProjectWindows/Project/Models/DummyDataSource.cs
@@ -11,9 +11,9 @@ namespace Project.Models
 
         public static Richting RichtingToegepasteInformatica = new Richting()
         {
-            Title="Toegepaste Informatica",
+            Title = "Toegepaste Informatica",
             Description = "De IT’er van vandaag is veel meer dan alleen maar een technische expert. Hij of zij moet tegelijkertijd een ondernemende, communicatieve en klantgerichte teamspeler zijn. IT is namelijk geëvolueerd van een zuiver technisch verhaal naar ’IT as a service and support tool’.",
-            Html=""
+            Html = ""
         };
 
         public static Richting RichtingOfficeManagement = new Richting()
@@ -21,6 +21,15 @@ namespace Project.Models
             Title = "Office Management",
             Description = "Je bent een doener en je houdt van uitdagingen. In een team ben jij diegene die organiseert en coördineert. Je weet van aanpakken en bent een punctuele multitasker. Voor jou moeten de zaken vooruitgaan, goed getimed en gepland. Talen zijn volledig je ding en je communiceert graag. Je blik is internationaal, je aanpak no-nonsense.",
             Html = ""
+        };
+
+        public static Campus Aalst = new Campus()
+        {
+            Uitleg = "Studeren aan HoGent campus Aalst betekent studeren in een gezellige provinciestad. Voor veel studenten is de HoGent in Aalst de campus vlakbij huis. De campus ligt dicht bij het centrum en de winkelstraat en is op wandelafstand van het station en de parking Keizershallen."
+            + "Als student ben je er nooit anoniem en vind je er onmiddellijk je weg.HoGent campus Aalst staat voor een kleinschalige,"
+            + "gerichte aanpak met veel aandacht voor het individu.Typerend zijn de kleine klasgroepen en het familiale karakter.Docenten gaan voor een persoonlijke aanpak en coachen heel gericht je competenties en talenten."
+            + "Campus Aalst is bovendien zeer actief en heeft zoals de andere campussen van faculteit Bedrijf en Organisatie een sterk netwerk van nationale en internationale bedrijven.Theorie en praktijk gaan op de HoGent altijd hand in hand.Uniek is de inbreng van de lokale ondernemers.Zij bieden interessante stageplaatsen en werken mee aan heel wat initiatieven.Voor een gemotiveerde student de ideale opstap naar een boeiende job in de regio."
+
         };
     }
 }
