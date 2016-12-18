06549ccc830864dcbe4d009f15dd54f082172575
Yannick <yannick.servranckx@hotmail.com>
2016-12-19 00:21:28 +0100

    Added klasse evenement

---
+++ ProjectWindows/Project/Models/Evenement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    class Evenement
+    {
+        public string Naam { get; set; }
+        public string Uitleg { get; set; }
+        public DateTime Datum { get; set; }
+        public int Startuur { get; set; }
+        public int Einduur { get; set; }
+    }
+}
